dbf4ebf8e336f64273f8ab36bcc5e4226ef17cc3
alexander <soriaalexander@outlook.com>
2026-02-03 22:35:01 -0300

    Hecho el responsive en la pagina principal

--- index.php
+++ index.php
@@ -4,14 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>To Do List</title>
-    <link rel="stylesheet" href="assets/css/style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Archivo+Black&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=SN+Pro:ital,wght@0,200..900;1,200..900&display=swap" rel="stylesheet">
-
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
     <div class="contenedor">
@@ -20,6 +15,5 @@
         <button class="button-start mt-5">Empezar</button>
         <p class="mt-3 ">ya tienes una cuenta? <a href="login.php" class="link-login"">Inicia Sesion</a></p>
     </div>
-
 </body>
 </html>
